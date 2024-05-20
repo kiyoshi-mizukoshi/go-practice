615c68f1e0c4054f30f3e5ba0bd1fd1632282dca
kiyoshi-mizukoshi <928kiyoshi@gmail.com>
2024-05-21 00:03:44 +0900

    add: ポインタ練習

--- main.go
+++ main.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"fmt"
-	"go-basics/calculator"
-	"os"
-
-	"github.com/joho/godotenv"
+	"unsafe"
 )
 
 func main() {
-	godotenv.Load()
-	fmt.Println(os.Getenv("GO_ENV"))
-	fmt.Println(calculator.Offset)
-	fmt.Println(calculator.Sum(1, 2))
-	fmt.Println(calculator.Multiply(1, 2))
-	PracticeVariables()
+	// godotenv.Load()
+	// fmt.Println(os.Getenv("GO_ENV"))
+	// fmt.Println(calculator.Offset)
+	// fmt.Println(calculator.Sum(1, 2))
+	// fmt.Println(calculator.Multiply(1, 2))
+
+	// PracticeVariables()
+
+	PracticePointer()
 }
 
 func PracticeVariables() {
@@ -51,4 +51,27 @@ func PracticeVariables() {
 	fmt.Printf("pi: %v title: %v\n", pi, title)
 
 	fmt.Printf("Mac:%v Windows:%v Linux:%v\n", Mac, Windows, Linux)
+}
+
+func PracticePointer() {
+	var ui1 uint16
+	fmt.Printf("memory address of ui1: %p\n", &ui1)
+	var ui2 uint16
+	fmt.Printf("memory address of ui2: %p\n", &ui2)
+	var p1 *uint16
+	fmt.Printf("value of p1: %v\n", p1)
+	p1 = &ui1
+	fmt.Printf("value of p1: %v\n", p1)
+	fmt.Printf("size of p1: %d[byte]\n", unsafe.Sizeof(p1))
+	fmt.Printf("memory address of p1: %p\n", &p1)
+	fmt.Printf("value of ui1(dereferenced): %v\n", *p1)
+	*p1 = 1
+	fmt.Printf("value of ui1: %v\n", ui1)
+
+	var pp1 **uint16 = &p1
+	fmt.Printf("value of pp1: %v\n", pp1)
+	fmt.Printf("size of pp1: %d[byte]\n", unsafe.Sizeof(pp1))
+	fmt.Printf("memory address of pp1: %p\n", &pp1)
+	fmt.Printf("value of p1(dereferenced): %v\n", *pp1)
+	fmt.Printf("value of ui1(dereferenced): %v\n", **pp1)
 }
