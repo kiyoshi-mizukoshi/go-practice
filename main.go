3f68b06989f3b6d606103d96800ee18be3afd74e
kiyoshi-mizukoshi <928kiyoshi@gmail.com>
2024-05-26 15:55:05 +0900

    add:slice練習

--- main.go
+++ main.go
@@ -14,7 +14,9 @@ func main() {
 
 	// PracticeVariables()
 
-	PracticePointer()
+	// PracticePointer()
+
+	PracticeSliceAndMap()
 }
 
 func PracticeVariables() {
@@ -74,4 +76,25 @@ func PracticePointer() {
 	fmt.Printf("memory address of pp1: %p\n", &pp1)
 	fmt.Printf("value of p1(dereferenced): %v\n", *pp1)
 	fmt.Printf("value of ui1(dereferenced): %v\n", **pp1)
+}
+
+func PracticeSliceAndMap() {
+	var a1 [3]int
+	var a2 = [3]int{10, 20, 30}
+	a3 := [...]int{10, 20}
+	fmt.Printf("%v %v %v\n", a1, a2, a3)
+	fmt.Printf("%v %v\n", len(a3), cap(a3))
+	fmt.Printf("%T %T\n", a2, a3)
+
+	var s1 []int
+	s2 := []int{}
+	fmt.Printf("s1: %[1]T %[1]v %v %v\n", s1, len(s1), cap(s1))
+	fmt.Printf("s2: %[1]T %[1]v %v %v\n", s2, len(s2), cap(s2))
+	fmt.Println(s1 == nil)
+	fmt.Println(s2 == nil)
+	s1 = append(s1, 1, 2, 3)
+	fmt.Printf("s1: %[1]T %[1]v %v %v\n", s1, len(s1), cap(s1))
+	s3 := []int{4, 5, 6}
+	s1 = append(s1, s3...)
+	fmt.Printf("s1: %[1]T %[1]v %v %v\n", s1, len(s1), cap(s1))
 }
